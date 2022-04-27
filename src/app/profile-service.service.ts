25e5d77e7719db309b918d406aec4336a85e78ec
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-27 21:49:16 +0300

    update promises

--- src/app/profile-service.service.ts
+++ src/app/profile-service.service.ts
@@ -9,6 +9,7 @@ export class ProfileServiceService {
   BaseUrl='https://api.github.com/users';
   constructor(private http:HttpClient) { }
   async getUser(userName:string){
-    let response=this.http.get('${this.BaseUrl}/${userName}')
+    let response=this.http.get('${this.BaseUrl}/${userName}');
+    return await lastValueFrom(response).then((result)=> result);
   }
 }
