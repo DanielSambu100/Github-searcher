c8be180f580e238bc1e5c61657fc07004637d4a3
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-27 21:40:30 +0300

    update API

--- src/app/profile-service.service.ts
+++ src/app/profile-service.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileServiceService {
 
-  
+  BaseUrl='https://api.github.com/users';
   constructor(private http:HttpClient) { }
+  async getUser(userName:string){
+    let response=this.http.get('${this.BaseUrl}/${userName}')
+  }
 }
