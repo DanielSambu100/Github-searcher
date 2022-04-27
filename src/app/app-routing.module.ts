5cd6abf6eae7e106fe8e344e411efb818ed2955d
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-27 20:36:58 +0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubReposComponent } from './github-repos/github-repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:GithubProfileComponent},
+  {path:'Repos',component:GithubReposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
